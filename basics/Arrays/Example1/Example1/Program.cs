9ac0cded039e0a09fa2eab20ca4d44d6fc406512
Shravan <shravan.kasagoni@outlook.com>
2015-08-20 03:17:24 +0530

    Arrays

---
+++ basics/Arrays/Example1/Example1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Declaring Arrays*/
+
+            int[] table; // not int table[];  
+
+
+            // Single-dimensional arrays:
+
+            int[] numbers;          // declare numbers as an int array of any size
+
+            numbers = new int[10];  // numbers is a 10-element array
+
+            numbers = new int[20];  // now it's a 20-element array
+
+
+            // Multidimensional arrays:
+
+            int[,] array = new int[4, 2];
+
+            int[,,] array1 = new int[4, 2, 3];
+
+            string[,] names = new string[2,3];
+
+            // Array-of-arrays (jagged):
+
+            byte[][] scores = new byte[5][];
+            for (int i = 0; i < scores.Length; i++)
+            {
+                scores[i] = new byte[i + 3];
+            }
+
+            // Print length of each row
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine("Length of row {0} is {1}", i, scores[i].Length);
+            }
+
+        }
+    }
+}
---
+++ basics/Arrays/Example2/Example2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initializing Arrays
+
+            int[] numbers1 = new int[5] { 1, 2, 3, 4, 5 };
+            string[] names1 = new string[3] { "one", "two", "three" };
+
+            //You can omit the size of the array:
+
+            int[] numbers2 = new int[] { 1, 2, 3, 4, 5 };
+            string[] names2 = new string[] { "one", "two", "three" };
+
+            // Multidimensional Array
+
+            int[,] numbers3 = new int[3, 2] 
+            { 
+                { 1, 2 }, 
+                { 3, 4 }, 
+                { 5, 6 }
+            };
+            
+            string[,] siblings1 = new string[2, 2] 
+            { 
+                { "Mike", "Amy" }, 
+                { "Mary", "Albert" }
+            };
+
+            //You can omit the size of the array, like this:
+
+            int[,] numbers4 = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            string[,] siblings2 = new string[,] { { "Mike", "Amy" }, { "Mary", "Albert" } };
+
+            // You can also omit the new operator
+
+            int[] numbers5 = { 1, 2, 3, 4, 5, 6 };
+
+            string[] names3 =  { "one", "two", "three", "four" };
+
+            int[,] numbers6 = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+            string[,] siblings3 = { { "Mike", "Amy" }, { "Mary", "Albert" } };
+
+            //print the values
+
+            Console.WriteLine(numbers1[3]);
+
+            Console.WriteLine(names1[2]);
+
+            Console.WriteLine(numbers3[2,1]);
+
+            Console.WriteLine("===FOR EACH===");
+
+            foreach (int i in numbers1)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("===FOR EACH===");
+            int[,] numbers7 = new int[3, 2] { { 9, 99 }, { 3, 33 }, { 5, 55 } };
+            foreach (int i in numbers7)
+            {
+                Console.Write("{0} ", i);
+            }
+            
+            Console.Write(Environment.NewLine + "Array Length: " + names1.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
