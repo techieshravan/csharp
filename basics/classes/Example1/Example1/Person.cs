1f929f0f7fe785d456da44d3fcd891f1a86fc92e
Shravan <shravan.kasagoni@outlook.com>
2015-08-20 01:26:25 +0530

    First Example
    Person class with properties

---
+++ basics/classes/Example1/Example1/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace Example1
+{
+    class Person
+    {
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        private byte _age;
+
+        public byte Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+    }
+}
---
+++ basics/classes/Example1/Example1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            person1.FirstName = "Shravan";
+            person1.LastName = "Kasagoni";
+            person1.Age = 26;
+
+            Console.WriteLine("Person Name: " + person1.FirstName + " " + person1.LastName);
+            Console.WriteLine("Person Age: " + person1.Age);
+
+            Console.ReadLine();
+        }
+    }
+}
