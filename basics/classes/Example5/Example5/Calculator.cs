ea6b0a106db93cc0e908a9dbb61f897c738b7fb2
Shravan <shravan.kasagoni@outlook.com>
2015-08-20 02:10:53 +0530

    Calculator Example

---
+++ basics/classes/Example5/Example5/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Example5
+{
+    class Calculator
+    {
+        public double Add(int num1, int num2)
+        {
+            var sum = num1 + num2;
+            return sum;
+        }
+    
+        public double Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+       
+        public int Subtract(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1 - num2;
+            }
+            return num2 - num1;
+        }
+    }
+}
---
+++ basics/classes/Example5/Example5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Example5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            
+            Console.WriteLine(calculator.Multiply(10, 20));
+
+            Console.WriteLine(calculator.Subtract(30, 5));
+
+            Console.WriteLine(calculator.Subtract(5, 87));
+
+            Console.WriteLine(calculator.Add(100, 3000000));
+
+            Console.ReadLine();
+        }
+    }
+}
