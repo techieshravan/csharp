c1b9e6693f3bc5f7e65da91d64976a2686a206b0
Shravan <shravan.kasagoni@outlook.com>
2015-08-20 03:50:06 +0530

    Parameters

---
+++ basics/classes/Example10/Example10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+
+            Console.WriteLine("a value in main before calling foo1: " + a);
+
+            Foo1(ref a);
+
+            Console.WriteLine("a value in main after calling foo1: " + a);
+
+            Console.ReadKey();
+        }
+
+        private static void Foo1(ref int x)
+        {
+            Console.WriteLine("a in foo1 before increment: " + x);
+            x++;
+            Console.WriteLine("a in foo1 after increment: " + x);
+        }
+    }
+}
---
+++ basics/classes/Example11Output Parameters/Example11Output Parameters/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Example11Output_Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number: ");
+            var number = Convert.ToInt16(Console.ReadLine());
+
+            double rootValue;
+
+            var result = TrySqrt(number, out rootValue);
+
+            if(result)
+            {
+                Console.Write("Square Root of {0} is {1}" , number, rootValue);
+            }
+            else
+            {
+                Console.Write("input number should be greater than zero!");
+            }
+
+            Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+
+            string input = Console.ReadLine();
+
+            int age;
+
+            if (!int.TryParse(input, out age))
+                Console.Write("Invaild input for age");
+            else
+                Console.Write("Valid age");
+
+            Console.ReadKey();
+        }
+
+        static bool TrySqrt(int input, out double root)
+        {
+            root = 0.0;
+
+            if (input < 0)
+                return false;
+
+            root = Math.Sqrt(input);
+
+            return true;
+        }
+    }
+}
