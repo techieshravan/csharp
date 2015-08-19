477fd8481b2863415bd3475cd4df3f34fe8c1df6
Shravan <shravan.kasagoni@outlook.com>
2015-08-20 01:33:36 +0530

    C# Auto-implemented properties

---
+++ basics/classes/Example2/Example2/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Example2
+{
+    class Person
+    {   
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
