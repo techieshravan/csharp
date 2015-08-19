24a0a1b3805ccbc86e436cdf61b900bfd18d3a39
Shravan <shravan.kasagoni@outlook.com>
2015-08-20 02:35:37 +0530

    Constructor Params

---
+++ basics/classes/Example8ConstructorParams/Example8ConstructorParams/Coordinates.cs
@@ -0,0 +1,18 @@
+﻿namespace Example8ConstructorParams
+{
+    class Coordinates
+    {
+        public int x, y;
+
+        public Coordinates(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", x, y);
+        }
+    }
+}
---
+++ basics/classes/Example8ConstructorParams/Example8ConstructorParams/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example8ConstructorParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coords = new Coordinates(10, 50);
+
+            Console.WriteLine("Coordinates: {0}", coords);
+
+            Console.ReadKey();
+        }
+    }
+}
