e8428c7c2efd61536a2a44c40f1b7542e2fff089
Shravan <shravan.kasagoni@outlook.com>
2015-08-20 01:46:35 +0530

    methods example2

---
+++ basics/classes/Example4/Example4/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example4
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public void SayName()
+        {
+            var name = string.Format("Name: {0} {1}", FirstName, LastName);
+
+            Console.WriteLine(name);
+        }
+
+        public void SayAge()
+        {
+            Console.WriteLine(Age);
+        }
+    }
+}
---
+++ basics/classes/Example4/Example4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Example4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            person1.FirstName = "Shravan";
+            person1.LastName = "Kasagoni";
+            person1.Age = 26;
+
+            person1.SayName();
+            person1.SayAge();
+
+            Console.ReadLine();
+        }
+    }
+}
