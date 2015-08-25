28e52a3ea31df023eb33c43d5c07fedc569e456d
Shravan <shravan.kasagoni@outlook.com>
2015-08-26 01:23:45 +0530

    Multiple-Document Interface (MDI) Application

---
+++ Windows Forms/Application8/Application8/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Application8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var newMdiChild = new Form2
+            {
+                MdiParent = this
+            };
+            newMdiChild.Show();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                var file = openFileDialog1.FileName;
+                try
+                {
+                    var text = File.ReadAllText(file);
+                    var newMdiChild = new Form2
+                    {
+                        MdiParent = this,
+                        Text = Path.GetFileNameWithoutExtension(file)
+                    };
+                    newMdiChild.Controls["richTextBox1"].Text = text;
+                    newMdiChild.Show();
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+    }
+}
