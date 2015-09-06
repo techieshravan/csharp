48a69bab7169db4787148c3600ec039b4b20f16e
Shravan <shravan.kasagoni@outlook.com>
2015-09-07 00:28:20 +0530

    reading books.xml

---
+++ XML/Application7/Application7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Application7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            Console.WriteLine("Parsing XML from String\n");
+
+            var xml = @"<Contacts>
+                            <Contact>
+                                <Name>Patrick Hines</Name>
+                                <Phone Type=""home"">206-555-0144</Phone>
+                                <Phone type=""work"">425-555-0145</Phone>
+                                <Address>
+                                <Street1>123 Main St</Street1>
+                                <City>Mercer Island</City>
+                                <State>WA</State>
+                                <Postal>68042</Postal>
+                                </Address>
+                                <NetWorth>10</NetWorth>
+                            </Contact>
+                            <Contact>
+                                <Name>Gretchen Rivas</Name>
+                                <Phone Type=""mobile"">206-555-0163</Phone>
+                                <Address>
+                                <Street1>123 Main St</Street1>
+                                <City>Mercer Island</City>
+                                <State>WA</State>
+                                <Postal>68042</Postal>
+                                </Address>
+                                <NetWorth>11</NetWorth>
+                            </Contact>
+                        </Contacts>";
+            
+            XElement contacts = XElement.Parse(xml);
+            Console.WriteLine(contacts);
+
+            Console.ReadKey();
+
+            Console.WriteLine("\nLoad XML from a File\n");
+            XElement booksFromFile = XElement.Load(@"books.xml");
+            Console.WriteLine(booksFromFile);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ XML/Application8/Application8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Application8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (XmlReader reader = XmlReader.Create("books.xml"))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        Console.WriteLine("Element: {0}", reader.Name);
+
+
+                        string attribute = reader["id"];
+                        if (attribute != null)
+                        {
+                            Console.WriteLine(reader.Name + " element has attribute id: " + attribute);
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+
+            XmlReader r = XmlReader.Create("books.xml");
+            while (r.NodeType != XmlNodeType.Element)
+                r.Read();
+            XElement e = XElement.Load(r);
+            Console.WriteLine(e);
+
+            Console.ReadKey();
+
+            /**
+             * The streaming .NET XML APIs (which support XmlReader and XmlWriter),
+             * which never attempt to hold the whole document in memory—you work one element at a time, 
+             * and while that enables you to handle very large documents without using much memory.
+             **/
+        }
+    }
+}
